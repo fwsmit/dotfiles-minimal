31707940eaf3b3ab27c53cb55267bcc80994411c
Friso Smit <fw.smit01@gmail.com>
2022-08-16 00:08:47 +0200

    Neovim: Add initial config

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,22 @@
+-- Set highlight on search. This will remove the highlight after searching for text.
+vim.o.hlsearch = false
+
+-- Make relative line numbers default. The current line number will be shown as well as relative numbering from that current line. It makes navigating around code easier.
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- Enable mouse mode. Sometimes it's easier to scroll using the touchpad for example.
+vim.o.mouse = 'a'
+
+-- Enable break indent.
+vim.o.breakindent = true
+
+-- Save undo history.
+vim.opt.undofile = true
+
+-- Case insensitive searching unless /C or capital in search.
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+
+require("plugins")
