e61d4855f05cb982e7f036b9bdca34b2cec4752b
Friso Smit <fw.smit01@gmail.com>
2022-09-28 14:31:34 +0200

    Add git rebase abbreviations

--- dot_config/private_fish/conf.d/git-abbreviations.fish
+++ dot_config/private_fish/conf.d/git-abbreviations.fish
@@ -1,6 +1,12 @@
 abbr -a -U -- ga 'git add'
+
 abbr -a -U -- gd 'git diff'
+
 abbr -a -U -- gc 'git commit -v'
 abbr -a -U -- gca 'git commit -av'
 abbr -a -U -- gcam 'git commit -v --amend'
+
 abbr -a -U -- gst 'git status'
+
+abbr -a -U -- grb 'git rebase'
+abbr -a -U -- grbc 'git rebase --continue'
