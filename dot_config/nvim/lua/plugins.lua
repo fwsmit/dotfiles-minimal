e307c05623ce3d338016b3ba850fead6b18dae6c
Friso Smit <fw.smit01@gmail.com>
2022-10-07 12:40:48 +0200

    Add stable nvim config

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -10,4 +10,11 @@ return require('packer').startup(function(use)
 	use 'nvim-treesitter/nvim-treesitter' -- Highlight, edit, and navigate code using a fast incremental parsing library. Treesitter is used by nvim for various things, but among others, for syntax coloring. Make sure that any themes you install support treesitter!
 	use 'nvim-treesitter/nvim-treesitter-textobjects' -- Additional textobjects for treesitter.
 	use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client.
+
+        -- cmp recommended config
+        use 'hrsh7th/cmp-nvim-lsp'
+        use 'hrsh7th/cmp-buffer'
+        use 'hrsh7th/cmp-path'
+        use 'hrsh7th/cmp-cmdline'
+        use 'hrsh7th/nvim-cmp'
 end)
