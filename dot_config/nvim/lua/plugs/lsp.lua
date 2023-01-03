baefa886b4bbfed6bf0b29e68bf45810a14eb3e1
Friso Smit <fw.smit01@gmail.com>
2023-01-03 16:54:41 +0100

    nvim: Setup lsp servers correctly

--- dot_config/nvim/lua/plugs/lsp.lua
+++ dot_config/nvim/lua/plugs/lsp.lua
@@ -1,2 +1,14 @@
 -- C++ and others language server
---require'lspconfig'.sourcekit.setup{}
+local lspconfig = require('lspconfig')
+
+-- nvim-cmp supports additional completion capabilities.
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    -- on_attach = my_custom_on_attach,
+    capabilities = capabilities,
+  }
+end
--- dot_config/nvim/lua/plugs/treesitter.lua
+++ dot_config/nvim/lua/plugs/treesitter.lua
@@ -57,15 +57,3 @@ require('nvim-treesitter.configs').setup {
     },
   },
 }
-
--- nvim-cmp supports additional completion capabilities.
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
--- Enable the following language servers. If you ever find yourself needing another programming language support, you'll have to find its LSP, add it to this list and make sure it is installed in your system! We'll go through installing tsserver together for TypeScript support.
---for _, lsp in ipairs(servers) do
-  --nvim_lsp[lsp].setup {
-    --on_attach = on_attach,
-    --capabilities = capabilities,
-  --}
---end
